033d6bf10efb28f4726a9a37d4532ac4ba73c58b
MohamedAnas97 <mohamed.a@gnginfotech.com>
2023-09-12 12:01:38 +0400

    update footer part

--- src/shared/layout/footer.tsx
+++ src/shared/layout/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         {/* footer up */}
         <div className="flex flex-col md:flex-row">
           <div className="w-full lg:w-[63%] pt-16 md:pb-32 pb-16 md:border-x-[1px] border-b-[1px] border-[#434345]">
-            <div className="w-full 2xl:w-[50%] md:w-[80%] px-8 ">
+            <div className="w-full 2xl:w-[60%] md:w-[90%] px-8 ">
               <h4 className="text-[29px] font-extralight aeionik-font text-[#fff] ">
                 Subscribe to our newsletter to stay in touch with the latest!
               </h4>
@@ -26,14 +26,11 @@ const Footer = () => {
               </div>
               <div className="py-3">
                 <label className="checkbox-container2 text-[15px] text-[#757575] font-extralight aeionik-font">
-                <input type="checkbox" />
-                  <span className="checkmark"></span>
-                  {" "}
-                  I agree to the{" "}
+                  <input type="checkbox" />
+                  <span className="checkmark"></span> I agree to the{" "}
                   <span className="underline">Privacy Policy</span> and give my
                   permission to process my personal data for the purposes
                   specified in the Privacy Policy
-                  
                 </label>
               </div>
             </div>
@@ -61,47 +58,51 @@ const Footer = () => {
         </div>
         {/* footer down */}
         <div className="flex flex-col-reverse md:flex-row">
-          <div className="w-full lg:w-[63%] md:py-16 py-6 md:border-x-[1px] border-b-[1px] border-[#434345]">
-            <div className="px-8 flex md:justify-start justify-center ">
+          <div className="w-full lg:w-[63%] md:py-16 py-6 md:border-x-[1px] border-b-[1px] border-[#434345] ">
+            <div className="px-8 flex md:justify-start justify-center 2xl:pt-52">
               <Image src={Logo} alt="" className="md:w-60 w-48 md:h-12 h-10" />
             </div>
           </div>
           {/* social link start here */}
           <div className="lg:w-[37%] w-full md:py-16 py-6 border-b-[1px] border-[#434345]">
             <div className="flex 2xl:flex-row flex-col justify-between">
-              <ul className="px-8 text-center md:text-left">
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
-                  Linkedin
-                </li>
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
-                  Instagram
-                </li>
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
-                  Facebook
-                </li>
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
-                  Youtube
-                </li>
-              </ul>
-              <ul className="px-8 text-center md:text-left">
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
-                  UAE: +971 (04) 334-9500
-                </li>
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
-                  1113, Grosvenor Business Tower, Barsha Heights
-                </li>
-                <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
-                  Dubai, UAE
-                </li>
-              </ul>
+              <div className="2xl:w-[25%] w-full">
+                <ul className="px-8 text-center md:text-left">
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
+                    Linkedin
+                  </li>
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
+                    Instagram
+                  </li>
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
+                    Facebook
+                  </li>
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 cursor-pointer">
+                    Youtube
+                  </li>
+                </ul>
+              </div>
+              <div className="2xl:w-[60%] w-full">
+                <ul className="px-8 text-center md:text-left">
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
+                    UAE: +971 (04) 334-9500
+                  </li>
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
+                    1113, Grosvenor Business Tower, Barsha Heights
+                  </li>
+                  <li className="text-[18px] text-[#fff] font-extralight aeionik-font py-3 ">
+                    Dubai, UAE
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="flex 2xl:flex-row flex-col justify-between px-8">
-              <div className="text-center md:text-left">
+              <div className="text-center md:text-left 2xl:pt-8">
                 <h6 className="text-[16px] text-[#fff] font-extralight aeionik-font py-3 ">
                   © 2023. G&G Infotech. All Rights Reserved.
                 </h6>
               </div>
-              <div className="flex md:justify-start justify-center">
+              <div className="flex md:justify-start justify-center 2xl:pt-8">
                 <button className="text-[16px] text-[#fff] font-extralight aeionik-font py-3 underline">
                   Privacy Policy
                 </button>
